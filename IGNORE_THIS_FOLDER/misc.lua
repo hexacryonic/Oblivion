b28d2df90592f0c6fa4308d2eb0dc7522aef6d29
Oinite <tinioalfred@gmail.com>
2025-07-26 18:26:17 -0600

    did a little bit for cryptid crossmod

--- IGNORE_THIS_FOLDER/misc.lua
+++ IGNORE_THIS_FOLDER/misc.lua
@@ -6,106 +6,64 @@ local cryptid_crossplanet_atlas = {
 	py = 95,
 }
 
+local fucking_huge = {
+	chips = to_big(65656565656565656565656565656565656565656565)^65656565656565656565656565,
+	mult = to_big(6565656565656565656565656565656565656565656)^65656565656565656565656565,
+	l_chips = to_big(6565656565656565656565656565656565656565656)^65656565656565656565656565,
+	l_mult = to_big(656565656565656565656565656565656565656565)^65656565656565656565656565
+}
+
+local function tefdats_example()
+	local cards = {}
+	local suits = {"ovn_O", "S", "H", "C", "D"}
+	local ranks = {"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"}
+
+	for _,rank in ipairs(ranks) do
+		for _,suit in ipairs(suits) do
+			table.insert(cards, {suit .. "_" .. rank, true})
+		end
+	end
+
+	return cards
+end
+
 local ref = Game.start_run
 function Game:start_run(args)
 	ref(self, args)
 	if not args.savetext then
-		G.GAME.hands.ovn_5DDeck.mult = to_big(6565656565656565656565656565656565656565656)^65656565656565656565656565
-		G.GAME.hands.ovn_5DDeck.l_mult = to_big(656565656565656565656565656565656565656565)^65656565656565656565656565
-		G.GAME.hands.ovn_5DDeck.chips = to_big(65656565656565656565656565656565656565656565)^65656565656565656565656565
-		G.GAME.hands.ovn_5DDeck.l_chips = to_big(6565656565656565656565656565656565656565656)^65656565656565656565656565
+		G.GAME.hands.ovn_5DDeck.mult = fucking_huge.mult
+		G.GAME.hands.ovn_5DDeck.l_mult = fucking_huge.l_mult
+		G.GAME.hands.ovn_5DDeck.chips = fucking_huge.chips
+		G.GAME.hands.ovn_5DDeck.l_chips = fucking_huge.l_chips
 	end
 end
 
 local tefdats = {
 	object_type = "PokerHand",
 	key = "5DDeck",
-	visible = false,
-	chips = to_big(65656565656565656565656565656565656565656565)^65656565656565656565656565,
-	mult = to_big(6565656565656565656565656565656565656565656)^65656565656565656565656565,
-	l_chips = to_big(6565656565656565656565656565656565656565656)^65656565656565656565656565,
-	l_mult = to_big(656565656565656565656565656565656565656565)^65656565656565656565656565,
-	example = {
-	{ "ovn_O_A", true },
-		{ "S_A", true },
-		{ "H_A", true },
-		{ "C_A", true },
-		{ "D_A", true },
-	{ "ovn_O_K", true },
-		{ "S_K", true },
-		{ "H_K", true },
-		{ "C_K", true },
-		{ "D_K", true },
-	{ "ovn_O_Q", true },
-		{ "S_Q", true },
-		{ "H_Q", true },
-		{ "C_Q", true },
-		{ "D_Q", true },
-	{ "ovn_O_J", true },
-		{ "S_J", true },
-		{ "H_J", true },
-		{ "C_J", true },
-		{ "D_J", true },
-	{ "ovn_O_T", true },
-		{ "S_T", true },
-		{ "H_T", true },
-		{ "C_T", true },
-		{ "D_T", true },
-	{ "ovn_O_9", true },
-		{ "S_9", true },
-		{ "H_9", true },
-		{ "C_9", true },
-		{ "D_9", true },
-	{ "ovn_O_8", true },
-		{ "S_8", true },
-		{ "H_8", true },
-		{ "C_8", true },
-		{ "D_8", true },
-	{ "ovn_O_7", true },
-		{ "S_7", true },
-		{ "H_7", true },
-		{ "C_7", true },
-		{ "D_7", true },
-	{ "ovn_O_6", true },
-		{ "S_6", true },
-		{ "H_6", true },
-		{ "C_6", true },
-		{ "D_6", true },
-	{ "ovn_O_5", true },
-		{ "S_5", true },
-		{ "H_5", true },
-		{ "C_5", true },
-		{ "D_5", true },
-	{ "ovn_O_4", true },
-		{ "S_4", true },
-		{ "H_4", true },
-		{ "C_4", true },
-		{ "D_4", true },
-	{ "ovn_O_3", true },
-		{ "S_3", true },
-		{ "H_3", true },
-		{ "C_3", true },
-		{ "D_3", true },
-	{ "ovn_O_2", true },
-		{ "S_2", true },
-		{ "H_2", true },
-		{ "C_2", true },
-		{ "D_2", true },
-	},
 	loc_txt = {
-			name = 'The Entire Fucking Deck And Then Some',
-			description = {
+		name = 'The Entire Fucking Deck And Then Some',
+		description = {
 			"A hand that contains every single",
 			"card found in a 52-card deck, plus",
 			"an entire full set of Optics",
 			" ",
 			"What the actual fuck is wrong with you?",
-			}
+		}
 	},
+	example = tefdats_example(),
+
+	visible = false,
+	chips = fucking_huge.chips,
+	mult = fucking_huge.mult,
+	l_chips = fucking_huge.l_chips,
+	l_mult = fucking_huge.l_mult,
+
 	evaluate = function(parts, hand)
 		if Cryptid.enabled("set_cry_poker_hand_stuff") ~= true or Cryptid.enabled("c_cry_universe") ~= true then
 			return
 		end
+		
 		if #hand >= 65 then
 			local deck_booleans = {}
 			local scored_cards = {}
