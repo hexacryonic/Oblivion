6a5e1947990410606ef488f8944a512855c6db95
Oinite <tinioalfred@gmail.com>
2025-08-04 20:42:21 -0600

    moved blind localization back to main file

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -67,6 +67,32 @@ local loc =  {
 			p_ovn_wicked_normal_3 = macro.p_ovn_wicked_normal
 		},
 
+		Blind = {
+			bl_ovn_nerve = {
+				name = 'The Nerve',
+				text = {
+					'All Optic cards',
+					'are debuffed'
+				}
+			},
+			bl_ovn_purity = {
+				name = 'The Purity',
+				text = {
+					'When defeated or disabled:',
+					'Purify all Corrupted Jokers',
+					'if any hands remain'
+				}
+			},
+			bl_ovn_stygian = {
+				name = 'Stygian Sigil',
+				text = {
+					'When entering Blind:',
+					'Corrupt all possible Jokers and',
+					'convert enhanced cards to Optics'
+				}
+			},
+		},
+
 		Edition = {
 			e_ovn_miasma = {
 				name = "Miasma",
