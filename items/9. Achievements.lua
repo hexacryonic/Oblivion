84d2656fcb029da06d62935350db3414544d158c
Oinite <tinioalfred@gmail.com>
2025-08-10 00:47:38 -0600

    First batch of achievements

---
+++ items/9. Achievements.lua
@@ -0,0 +1,223 @@
+local cdeck_cond = function(deck, stake)
+	stake = stake or 5
+	local current_deck = G.GAME.selected_back.effect.center.key
+	local current_stake = G.GAME.stake
+	return current_deck == deck and current_stake >= stake
+end
+
+SMODS.Achievement{
+	key = "red_rum",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local has_spiral = Ovn_f.has_joker('j_ovn_sprial_of_addiction')
+			if (
+				cdeck_cond("b_red")
+				and has_spiral
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "blue_blitz",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local round_count = G.GAME.round
+			local hand_count = G.GAME.hands_played
+			if (
+				cdeck_cond("b_blue")
+				and round_count == hand_count
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "yellow_yearlong",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local money_count = G.GAME.dollars
+			if (
+				cdeck_cond("b_yellow")
+				and money_count == "365"
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "groundless_greenery",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local money_count = G.GAME.dollars
+			local used_seedmoney = G.GAME.used_vouchers.v_seed_money
+			local used_moneytree = G.GAME.used_vouchers.v_money_tree
+			if (
+				cdeck_cond("b_green")
+				and used_seedmoney
+				and used_moneytree
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "bleakest_blackout",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local max_joker_count = G.GAME.max_jokers
+			if (
+				cdeck_cond("b_black")
+				and max_joker_count <= 4
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "magic_malaise",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local tarot_use_count = (
+				G.GAME.consumeable_usage_total
+				and G.GAME.consumeable_usage_total.tarot
+				or 0
+			)
+			if (
+				cdeck_cond("b_magic")
+				and tarot_use_count == 0
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "negated_nebula",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local tarot_use_count = (
+				G.GAME.consumeable_usage_total
+				and G.GAME.consumeable_usage_total.planet
+				or 0
+			)
+			if (
+				cdeck_cond("b_nebula")
+				and tarot_use_count == 0
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "ghostly_gall",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local tarot_use_count = (
+				G.GAME.consumeable_usage_total
+				and G.GAME.consumeable_usage_total.spectral
+				or 0
+			)
+			if (
+				cdeck_cond("b_ghost")
+				and tarot_use_count == 0
+			) then return true end
+		end
+	end
+}
+
+-- absolved abandoment
+-- checkered changeling
+-- zodiac zenith
+
+SMODS.Achievement{
+	key = "painted_paladin",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local joker_count = #G.jokers.cards
+			if (
+				cdeck_cond("b_painted")
+				and joker_count >= 7
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "anticipated_anaglyphs",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local double_tag_count = 0
+			for _,tag in ipairs(G.GAME.tags) do
+				if tag.key == "tag_double" then
+					double_tag_count = double_tag_count + 1
+				end
+			end
+			if (
+				cdeck_cond("b_painted")
+				and double_tag_count >= 7
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "plasma_plight",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local blind_requirement = G.GAME.blind.chips
+			local score = G.GAME.chips
+			if (
+				cdeck_cond("b_plasma")
+				and score >= blind_requirement*20
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "eratic_eruption",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			if (
+				cdeck_cond("b_erratic")
+				and pseudorandom("eratic_eruption") < (1/8)
+			) then return true end
+		end
+	end
+}
+
+SMODS.Achievement{
+	key = "ocular_overseer",
+	unlock_condition = function(self, args)
+		if args.type == 'win' then
+			local optics_count = 0
+			for _,card in ipairs(G.deck.cards) do
+				if card.base.suit == "ovn_Optics" then
+					optics_count = optics_count + 1
+				end
+			end
+			if (
+				cdeck_cond("b_ovn_ocular", 8)
+				and optics_count >= 40
+			) then return true end
+		end
+	end
+}
+
+-- decoherent deity
+-- abyssal absolution
+-- autocannibalism
+
+SMODS.Achievement{
+	key = "ace_combat",
+	unlock_condition = function(self, args)
+		local has_pmo = Ovn_f.has_joker('j_ovn_pmo')
+		local has_pareidolia = Ovn_f.has_joker('j_ovn_pareidolia')
+		if has_pmo and has_pareidolia then return true end
+	end
+}
+
+-- singular strike
+-- exposed nerve
+-- do it first
+-- bananas
---
+++ localization/en-us/misc/achievement_descriptions.lua
@@ -0,0 +1,30 @@
+local c_deck = function(deck, stake)
+	return table.concat({"On", deck, "Deck with", stake or "Blue", (stake == "Gold" and "Stake," or "Stake or higher, "), "win "}," ")
+end
+
+return {
+	["ach_ovn_red_rum"]               = c_deck("Red") .. "without discarding whilst holding Spiral of Addiction",
+	["ach_ovn_blue_blitz"]            = c_deck("Blue") .. "whilst having beaten every played blind in 1 hand",
+	["ach_ovn_yellow_yearlong"]       = c_deck("Yellow") .. "with at least $365 in bank",
+	["ach_ovn_groundless_greenery"]   = c_deck("Green") .. "with Seed Money and Money Tree",
+	["ach_ovn_bleakest_blackout"]     = c_deck("Black") .. "whilst never having more than 4 Jokers at a time",
+	["ach_ovn_magic_malaise"]         = c_deck("Magic") .. "without using any Tarot cards",
+	["ach_ovn_negated_nebula"]        = c_deck("Nebula") .. "without using any Planet cards",
+	["ach_ovn_ghostly_gall"]          = c_deck("Ghost") .. "without using any Spectral cards",
+	["ach_ovn_absolved_abandonment"]  = c_deck("Abandoned") .. "whilst having played a Royal Flush during a Finisher Boss Blind",
+	["ach_ovn_checkered_changeling"]  = c_deck("Checkered") .. "whilst having played a Straight Spectrum during a Finisher Boss Blind",
+	["ach_ovn_zodiac_zenith"]         = c_deck("Zodiac") .. "whilst having used 20 different Tarot or Planet cards",
+	["ach_ovn_painted_paladin"]       = c_deck("Painted") .. "whilst holding 7 or more Jokers at once",
+	["ach_ovn_anticipated_anaglyphs"] = c_deck("Anaglyphs") .. "with at least 7 Double Tags stockpiled",
+	["ach_ovn_plasma_plight"]         = c_deck("Plasma") .. "with a score at least 20 times greater than the Finisher Boss Blind requirement",
+	["ach_ovn_erratic_eruption"]      = "On Erratic Deck with Blue Stake or higher, this achievement has a 1 in 8 chance of unlocking on win",
+	["ach_ovn_ocular_overseer"]       = c_deck("Ocular", "Gold") .. "with at least 40 Optics in your deck",
+	["ach_ovn_decoherent_deity"]      = c_deck("Decoherence", "Gold") .. "with 7 or less Mutations applied",
+	["ach_ovn_abyssal_absolution"]    = "Win every Corrupt and Supreme Deck on Gold Stake, then on Abyssal Deck with Gold Stake, defeat the Abyss Superboss",
+	["ach_ovn_autocannibalism"]       = "Store Supply Drop inside of itself",
+	["ach_ovn_ace_combat"]            = "Have Pareidolia and Prosopometamorphopsia at the same time",
+	["ach_ovn_singular_strike"]       = "Play a single 10 boosted to X5 Mult from Airstrike against a Boss Blind",
+	["ach_ovn_exposed_nerve"]         = "Disable The Nerve, then play a Straight Flush of Optics",
+	["ach_ovn_do_it_first"]           = "While The Purity is upcoming, obtain the pure versions of currently held Corrupt Jokers, then sell all Corrupt Jokers",
+	["ach_ovn_bananas"]               = "Naturally obtain Aeon Cavendish",
+}
