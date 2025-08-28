537a9b1c93d686f62b95df134f62abe8709f9a55
Oinite <tinioalfred@gmail.com>
2025-08-28 17:50:31 -0600

    A few visual bug fixes

--- lib/funcs.lua
+++ lib/funcs.lua
@@ -63,16 +63,15 @@ Ovn_f.corrupt_joker = function(card)
 	local corrupted_card_key = Oblivion.corruption_map[card_key]
 	G.jokers:remove_from_highlighted(card)
 
-    add_simple_event('after', 0.4, function()
-        G.GAME.corruptingJoker = true
-		local ability = card.ability
-
+	G.GAME.corruptingJoker = true
+	local ability = card.ability
+	
+	if corrupted_card_key ~= card_key then
+		card:set_ability(G.P_CENTERS[corrupted_card_key], false, true)
+	end
+    add_simple_event(nil, nil, function()
         play_sound("ovn_abyss")
-		if corrupted_card_key ~= card_key then
-			card:set_ability(G.P_CENTERS[corrupted_card_key])
-		end
         card:juice_up(0.3, 0.5)
-		
 
 		card.ability.ovn_former_form = card_key
 		card:calculate_joker{
@@ -156,14 +155,14 @@ Ovn_f.purify_joker = function(card)
 	)
 	G.jokers:remove_from_highlighted(card)
 
-    add_simple_event('after', 0.4, function()
-        G.GAME.purifyingJoker = true
-		local ability = card.ability
+	G.GAME.purifyingJoker = true
+	local ability = card.ability
 
+	if pure_card_key ~= card_key then
+		card:set_ability(G.P_CENTERS[pure_card_key])
+	end
+    add_simple_event(nil, nil, function()
         play_sound("ovn_pure")
-		if pure_card_key ~= card_key then
-			card:set_ability(G.P_CENTERS[pure_card_key])
-        end
 		card:juice_up(0.3, 0.5)
 
 		card:calculate_joker{
