1bb8553ae250f965c19ede1543e5c9f3aeca7905
Oinite <tinioalfred@gmail.com>
2025-08-30 16:25:11 -0600

    baseball card now corruptible

--- Oblivion.lua
+++ Oblivion.lua
@@ -76,6 +76,7 @@ cmap["j_ovn_nexus_point"]     = "j_ovn_nexus_point"
 cmap["j_supernova"]           = "j_ovn_event_horizon"
 cmap["j_constellation"]       = "j_ovn_event_horizon"
 cmap["j_midas_mask"]          = "j_ovn_philosophers_stone"
+cmap["j_baseball"]            = "j_ovn_cigarette_card"
 
 if not Oblivion.corruption_condition then Oblivion.corruption_condition = {} end
 Oblivion.corruption_condition["j_gros_michel"] = function()
