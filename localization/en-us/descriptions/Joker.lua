aef5f8cc82c612df580c50c37913fe6b8dac22d1
Oinite <tinioalfred@gmail.com>
2025-09-09 00:43:26 -0600

    small ice joker desc change

--- localization/en-us/descriptions/Joker.lua
+++ localization/en-us/descriptions/Joker.lua
@@ -42,7 +42,7 @@ return {
 		name = 'Ice Joker',
 		text = {
 			"This Joker gains {X:mult,C:white}+X#1# {} Mult",
-			"when an {C:attention}Ice Card {}degrades",
+			"when an {C:attention}Ice {}Card degrades",
 			"{C:inactive}(Currently {X:mult,C:white}X#2# {C:inactive} Mult)",
 			"This gain increases by {C:attention}+#3#",
 			"when an {C:attention}Ice Card {}fully melts"
