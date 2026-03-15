9fdb8f26abae1bae26a1663906a3d523469c5518
Oinite12 <100444544+Oinite12@users.noreply.github.com>
2026-03-14 22:15:01 -0600

    Gave they/it pronouns to Eidolon

--- cross-mod/cardpronouns.lua
+++ cross-mod/cardpronouns.lua
@@ -1,10 +1,22 @@
+-----------
+-- PRONOUNS
+-- They/It
+-----------
+CardPronouns.Pronoun { key = "they_it",
+    colour = HEX("A83EFF"),
+    text_colour = G.C.WHITE,
+    pronoun_table = { "They", "It" },
+    in_pool = function()
+        return false
+    end,
+}
+
 -----------------------------
 -- CARD PRONOUNS
 -- Playing Card
 -- King of Optics - They/Them
 -----------------------------
-CardPronouns.PlayingCardOverride {
-	key = "king_of_optics",
+CardPronouns.PlayingCardOverride { key = "king_of_optics",
 	suit = "ovn_Optics",
 	rank = "King",
 	strict = true,
@@ -16,8 +28,7 @@ CardPronouns.PlayingCardOverride {
 -- Playing Card
 -- Queen of Optics - They/Them
 ------------------------------
-CardPronouns.PlayingCardOverride {
-	key = "queen_of_optics",
+CardPronouns.PlayingCardOverride { key = "queen_of_optics",
 	suit = "ovn_Optics",
 	rank = "Queen",
 	strict = true,
@@ -29,9 +40,17 @@ CardPronouns.PlayingCardOverride {
 -- Playing Card
 -- all other Optics - It/Its
 ----------------------------
-CardPronouns.PlayingCardOverride {
-	key = "all_optics",
+CardPronouns.PlayingCardOverride { key = "all_optics",
 	suit = "ovn_Optics",
 	strict = false,
 	pronoun = "it_its",
 }
+
+--------------------
+-- CARD PRONOUNS
+-- Consumable
+-- Eidolon - They/It
+--------------------
+SMODS.Consumable:take_ownership("c_ovn_eidolon", {
+	pronouns = "they_it"
+})
