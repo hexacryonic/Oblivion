6dab5c0b3b44b5344d82906549de7cf4e628945e
Oinite12 <100444544+Oinite12@users.noreply.github.com>
2026-05-24 18:45:43 -0600

    Extended vanilla lsp aliases

---
+++ modules/lsp_def.d.lua
@@ -0,0 +1,36 @@
+---@meta
+---@diagnostic disable: duplicate-doc-alias
+
+---@alias Enhancements
+---| 'm_ovn_radiant'
+---| 'm_ovn_dynamo'
+---| 'm_ovn_coord'
+---| 'm_ovn_ice'
+---| 'm_ovn_unob'
+---| 'm_ovn_crystal'
+---| 'm_ovn_dense'
+---| 'm_ovn_ion'
+
+---@alias Seals
+---| 'ovn_indigo'
+---| 'ovn_ruby_mark'
+---| 'ovn_sapphire_mark'
+---| 'ovn_citrine_mark'
+---| 'ovn_amethyst_mark'
+---| 'ovn_iolite_mark'
+
+---@alias Editions
+---| 'e_ovn_miasma'
+
+---@alias PokerHands
+---| 'ovn_Spectrum'
+---| 'ovn_Straight Spectrum'
+---| 'ovn_Spectrum House'
+---| 'ovn_Spectrum Five'
+
+---@alias Suits
+---| 'ovn_Optics'
+
+---@alias Rarities
+---| 'ovn_corrupted'
+---| 'ovn_supercorrupted'
